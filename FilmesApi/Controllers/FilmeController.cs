4d2c1555e40a05617202531a90314a96bef74736
AndreSilva <tadsturmab2014@gmail.com>
2022-05-28 10:52:45 -0300

    Classe e controle filme

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,24 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesApi.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        public static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody]Filme filme )
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
+    }
+}
