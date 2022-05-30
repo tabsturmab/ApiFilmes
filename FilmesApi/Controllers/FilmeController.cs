f0935315f6737e7a999fd4f10fca7b8b417c1340
AndreSilva <tadsturmab2014@gmail.com>
2022-05-30 16:07:06 -0300

    Metodos adicionar e recuperar filmes

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -13,12 +13,31 @@ namespace FilmesApi.Controllers
     public class FilmeController : ControllerBase
     {
         public static List<Filme> filmes = new List<Filme>();
+        public static int Id = 1;
 
         [HttpPost]
-        public void AdicionaFilme([FromBody]Filme filme )
+        public IActionResult AdicionaFilme([FromBody]Filme filme )
         {
+            filme.Id = Id++;
             filmes.Add(filme);
-            Console.WriteLine(filme.Titulo);
+            return CreatedAtAction(nameof(RecuperarFilmePorId), new { Id = filme.Id }, filme);
+        }
+
+        [HttpGet]
+        public IActionResult RecuperarFilmes()
+        {
+            return Ok(filmes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperarFilmePorId (int id)
+        {
+            Filme filme =  filmes.FirstOrDefault(filme => filme.Id == id);
+            if(filme != null)
+            {
+                return Ok(filme);
+            }
+            return NotFound();
         }
     }
 }
--- FilmesApi/Models/Filme.cs
+++ FilmesApi/Models/Filme.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,14 @@ namespace FilmesApi.Models
 {
     public class Filme
     {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O campo título é obrigatório")]
         public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo diretor é obrigatório")]
         public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage ="O campo gênero de ter no máximo 30 caracteres")]
         public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage = "A duração deve ter entre 1 a 600 minutos")]
         public int Duracao { get; set; }
     }
 }
